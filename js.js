b1195905e218267b5d2dd79c96751279fc2da0f5
AyvanL <ayvanzlopez@gmail.com>
2024-11-12 04:23:48 +0800

    add a lot of things

---
+++ js.js
@@ -0,0 +1,4 @@
+document.getElementById('popupButton').addEventListener('click', function() {
+    const popupDiv = document.getElementById('popupDiv');
+    popupDiv.classList.toggle('hidden'); // Toggle the visibility of the div
+});
